a84d73f874b2de3506d7bce92a36c41f9cceae8b
Filin1985 <ihsmen@gmail.com>
2021-01-16 11:04:05 +0300

    Add app pages

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {}
+    return render(request, 'pages/index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'pages/about.html', context)
